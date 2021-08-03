ab306aa38685c767e46f5fb3ad8e84647316a7e7
SmartInsider <smartyinsider@gmail.com>
2021-08-03 13:23:25 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" class="main">
+    <div class="container">
+      <div class="main-wr">
+        <SidebarLeft/>
+        <div class="main-center">
+          <router-view></router-view>
+        </div>
+        <SidebarRight/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SidebarLeft from '@/components/SidebarLeft.vue'
+import SidebarRight from '@/components/SidebarRight.vue'
+import VueStickyDirective from '@renatodeleao/vue-sticky-directive'
+export default {
+  name: 'App',
+  components: { SidebarLeft, SidebarRight },
+  directives: {
+    sticky: VueStickyDirective
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
