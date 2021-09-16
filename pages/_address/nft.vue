81795cc56a0902f984f1b0667259bd0d30c79973
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2021-09-16 18:03:31 +0400

    Create blank tokens and nft pages

---
+++ pages/_address/nft.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="posts">
+    <div>At the moment NFT is not displayed.</div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
---
+++ pages/_address/tokens.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="transactions">
+    <transactions :address="$route.params.address" :types="types" />
+  </div>
+</template>
+<script>
+export default {
+  components: {
+    transactions: () => import('~/components/AddressTransactionList.vue')
+  },
+  data: () => ({
+    types: ['tokens']
+  })
+}
+</script>
