2c8a5feda3ab09c50515ae12191057018f2d47ac
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2021-09-17 14:27:14 +0400

    Update nft, transactions, tokens  _address pages

--- pages/_address/tokens.vue
+++ pages/_address/tokens.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="transactions">
-    <transactions :address="$route.params.address" :types="types" />
+    <transactions
+      :address="$route.params.address"
+      type="tokens"
+      :total="transactionsCount"
+    >
+      <transaction
+        slot="transaction"
+        slot-scope="{ transaction }"
+        :transaction="transaction"
+      />
+    </transactions>
   </div>
 </template>
 <script>
 export default {
   components: {
+    transaction: () => import('~/components/ERC20Transaction.vue'),
     transactions: () => import('~/components/AddressTransactionList.vue')
   },
-  data: () => ({
-    types: ['tokens']
-  })
+  props: {
+    transactionsCount: {
+      type: Number,
+      default: () => 0
+    }
+  }
 }
 </script>
