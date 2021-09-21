d5b9845f6df9561a14ef9ba5b3161216dbe7ab30
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2021-09-21 14:53:03 +0400

    Create UserAvatar component

---
+++ components/UserAvatar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nuxt-link :to="`/${address}`">
+    <img
+      v-if="imageURL"
+      :src="imageURL"
+      :width="`${diameter}`"
+      :height="`${diameter}`"
+    />
+    <jazzicon :seed="10211" :diameter="diameter" :address="address" />
+    <span v-if="showAddress">{{ address | shortAddress(5, 7) }}</span>
+  </nuxt-link>
+</template>
+<script>
+export default {
+  props: {
+    address: {
+      type: String,
+      default: () => ''
+    },
+    imageURL: {
+      type: String,
+      default: () => ''
+    },
+    showAddress: {
+      type: Boolean,
+      default: () => true
+    },
+    diameter: {
+      type: Number,
+      default: () => 25
+    }
+  }
+}
+</script>
