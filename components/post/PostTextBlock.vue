a350f4d4315619eecdddb8c6e035c3f5fc3fc717
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2021-10-04 16:47:42 +0400

    Create PostTextBlock component

---
+++ components/post/PostTextBlock.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="post-text-block">
+    <div class="post-text">{{ text }}</div>
+    <div class="post-text-right">
+      <a href="#" class="post-text__link">
+        <img src="@/assets/img/post-text__link_img1_2.png" alt="" />
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
