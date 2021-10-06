b8dac3214b59917906e561ed92eec6598ddba4cc
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2021-10-06 23:04:47 +0400

    Remove props in AddressProfile component

--- components/address/AddressProfile.vue
+++ components/address/AddressProfile.vue
@@ -1,23 +1,6 @@
 <template>
   <div class="profile">
-    <address-profile-top
-      :address="address"
-      :transactions-count="transactionsCount"
-    />
+    <address-profile-top />
     <address-profile-bottom />
   </div>
 </template>
-<script>
-export default {
-  props: {
-    address: {
-      type: String,
-      required: true
-    },
-    transactionsCount: {
-      type: Number,
-      required: true
-    }
-  }
-}
-</script>
