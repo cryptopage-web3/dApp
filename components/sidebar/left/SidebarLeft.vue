e506047a6c168224d6f6cc9cca152029f44aba2e
Nail Mustyukov <nail.mustyukov@kamtor.com>
2021-10-06 10:51:12 +0500

    CRYP-43 layout: add js effects

--- components/sidebar/left/SidebarLeft.vue
+++ components/sidebar/left/SidebarLeft.vue
@@ -5,9 +5,9 @@
         <img src="@/assets/img/header-logo_img.png" />
         <div class="header-logo__text"><span>Crypto.</span>page</div>
       </router-link>
-      <router-link to="/" class="header-toggle d-xl-none">
+      <a href="#" class="header-toggle d-xl-none" @click.prevent="toggleMenu">
         <img src="@/assets/img/nav_bg2.svg" />
-      </router-link>
+      </a>
       <SidebarLeftMenu v-if="isAuth" />
       <SidebarLeftBanner v-else />
     </header>
@@ -37,6 +37,29 @@ export default {
       resizeSensor: true,
       minWidth: 1199.5
     })
+
+    // Hide mobile menu
+
+    document.addEventListener('click', function (event) {
+      const e = $('.header-list')
+      for (let i = 0; i < e.length; i++) {
+        if (
+          !e.get(i).contains(event.target) &&
+          !$('.header-toggle').get(0).contains(event.target)
+        ) {
+          $(e.get(i)).slideUp(300)
+        }
+      }
+    })
+  },
+  methods: {
+    toggleMenu() {
+      if ($('.header-list').is(':visible')) {
+        $('.header-list').slideUp(300)
+      } else {
+        $('.header-list').slideDown(300)
+      }
+    }
   }
 }
 </script>
---
+++ components/sidebar/right/SidebarRightConnect.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="connect-wallet-wr">
+    <div ref="connect" class="connect-wallet-col connect-wallet__link-hover">
+      <a
+        href="#"
+        data-toggle="modal"
+        data-target="#modal-connect"
+        role="button"
+        class="connect-wallet__link"
+      >
+        <div class="connect-wallet__link-thumb">
+          <img src="@/assets/img/connect-wallet__link_img2.png" alt="" />
+        </div>
+        <div class="connect-wallet__link-text">
+          <div class="connect-wallet__link-tool">Etherum</div>
+          <div class="connect-wallet__link-status">Connect wallet</div>
+        </div>
+      </a>
+      <div class="connect-wallet-col-body">
+        <ul class="connect-wallet__list">
+          <li><a href="#">Activate layer 2</a></li>
+          <li><a href="#">Copy Address</a></li>
+          <li><a href="#">Transaction history</a></li>
+          <li><a href="#">Claim</a></li>
+          <li><a href="#">Change Wallet</a></li>
+          <li><a href="#">Disconnect</a></li>
+        </ul>
+      </div>
+    </div>
+    <a href="#" class="dark-white">
+      <img src="@/assets/img/dark-white_img2.png" alt="" />
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  mounted() {
+    $(this.$refs.connect).hover(
+      function () {
+        $(this).addClass('active')
+        $(this).find('.connect-wallet-col-body').stop(true, true).slideDown(300)
+      },
+      function () {
+        $(this).removeClass('active')
+        $(this).find('.connect-wallet-col-body').slideUp(300)
+      }
+    )
+  }
+}
+</script>
