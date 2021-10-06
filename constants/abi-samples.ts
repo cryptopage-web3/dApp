709d7a151c0def45354057ecf41b61ccf5703c30
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2021-10-06 23:42:14 +0400

    Update abi-samples

--- constants/abi-samples.ts
+++ constants/abi-samples.ts
@@ -1,12 +1,54 @@
 import { AbiItem } from 'web3-utils'
-
-export const balanceOf: AbiItem = {
+export const name: AbiItem = {
   constant: true,
-  inputs: [{ name: '_owner', type: 'address' }],
-  name: 'balanceOf',
-  outputs: [{ name: 'balance', type: 'uint256' }],
+  inputs: [],
+  name: 'name',
+  outputs: [{ name: '', type: 'string' }],
   payable: false,
   stateMutability: 'view',
+  type: 'function'
+}
+export const approve: AbiItem = {
+  constant: false,
+  inputs: [
+    { name: '_spender', type: 'address' },
+    { name: '_value', type: 'uint256' }
+  ],
+  name: 'approve',
+  outputs: [{ name: '', type: 'bool' }],
+  payable: false,
+  stateMutability: 'nonpayable',
+  type: 'function'
+}
+export const totalSupply: AbiItem = {
+  constant: true,
+  inputs: [],
+  name: 'totalSupply',
+  outputs: [{ name: '', type: 'uint256' }],
+  payable: false,
+  stateMutability: 'view',
+  type: 'function'
+}
+export const transferFrom: AbiItem = {
+  constant: false,
+  inputs: [
+    {
+      name: '_from',
+      type: 'address'
+    },
+    {
+      name: '_to',
+      type: 'address'
+    },
+    {
+      name: '_value',
+      type: 'uint256'
+    }
+  ],
+  name: 'transferFrom',
+  outputs: [{ name: '', type: 'bool' }],
+  payable: false,
+  stateMutability: 'nonpayable',
   type: 'function'
 }
 export const decimals: AbiItem = {
@@ -18,7 +60,114 @@ export const decimals: AbiItem = {
   stateMutability: 'view',
   type: 'function'
 }
-
+export const balanceOf: AbiItem = {
+  constant: true,
+  inputs: [{ name: '_owner', type: 'address' }],
+  name: 'balanceOf',
+  outputs: [{ name: 'balance', type: 'uint256' }],
+  payable: false,
+  stateMutability: 'view',
+  type: 'function'
+}
+export const symbol: AbiItem = {
+  constant: true,
+  inputs: [],
+  name: 'symbol',
+  outputs: [{ name: '', type: 'string' }],
+  payable: false,
+  stateMutability: 'view',
+  type: 'function'
+}
+export const transfer: AbiItem = {
+  constant: false,
+  inputs: [
+    {
+      name: '_to',
+      type: 'address'
+    },
+    {
+      name: '_value',
+      type: 'uint256'
+    }
+  ],
+  name: 'transfer',
+  outputs: [{ name: '', type: 'bool' }],
+  payable: false,
+  stateMutability: 'nonpayable',
+  type: 'function'
+}
+export const allowance: AbiItem = {
+  constant: true,
+  inputs: [
+    {
+      name: '_owner',
+      type: 'address'
+    },
+    {
+      name: '_spender',
+      type: 'address'
+    }
+  ],
+  name: 'allowance',
+  outputs: [
+    {
+      name: '',
+      type: 'uint256'
+    }
+  ],
+  payable: false,
+  stateMutability: 'view',
+  type: 'function'
+}
+export const payable: AbiItem = {
+  payable: true,
+  stateMutability: 'payable',
+  type: 'fallback'
+}
+export const Approval: AbiItem = {
+  anonymous: false,
+  inputs: [
+    {
+      indexed: true,
+      name: 'owner',
+      type: 'address'
+    },
+    {
+      indexed: true,
+      name: 'spender',
+      type: 'address'
+    },
+    {
+      indexed: false,
+      name: 'value',
+      type: 'uint256'
+    }
+  ],
+  name: 'Approval',
+  type: 'event'
+}
+export const Transfer: AbiItem = {
+  anonymous: false,
+  inputs: [
+    {
+      indexed: true,
+      name: 'from',
+      type: 'address'
+    },
+    {
+      indexed: true,
+      name: 'to',
+      type: 'address'
+    },
+    {
+      indexed: false,
+      name: 'value',
+      type: 'uint256'
+    }
+  ],
+  name: 'Transfer',
+  type: 'event'
+}
 export const tokenURI: AbiItem = {
   constant: true,
   inputs: [{ name: 'tokenId', type: 'uint256' }],
@@ -38,5 +187,15 @@ export const ownerOf: AbiItem = {
   type: 'function'
 }
 
-export const ERC20ABI: AbiItem[] = [balanceOf, decimals]
+export const ERC20ABI: AbiItem[] = [
+  name,
+  approve,
+  totalSupply,
+  transferFrom,
+  decimals,
+  balanceOf,
+  symbol,
+  transfer,
+  allowance
+]
 export const ERC721ABI: AbiItem[] = [tokenURI, ownerOf]
