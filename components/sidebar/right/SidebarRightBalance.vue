6023fba6fe8d4240bec6c5113c981ea5f6ab94e8
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2021-10-06 23:41:20 +0400

    Create SidebarRightBalance component

---
+++ components/sidebar/right/SidebarRightBalance.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="balances">
+    <a
+      v-for="token in $store.getters['auth/tokens']"
+      :key="token.tokenInfo.address"
+      href="#"
+      class="balance"
+    >
+      <div class="balance-top">
+        <div class="balance__thumb">
+          <img :src="`https://ethplorer.io${token.tokenInfo.image}`" alt="" />
+        </div>
+        <div class="balance-right">
+          <div class="balance__countusdt">
+            {{
+              token.tokenInfo && token.tokenInfo.price
+                ? token.tokenInfo.price.rate.toFixed(2)
+                : 0
+            }}
+            $
+          </div>
+          <div
+            v-if="token.tokenInfo.price && token.tokenInfo.price.diff"
+            class="balance__procent"
+          >
+            {{ token.tokenInfo.price.diff }} %
+            <img
+              v-if="token.tokenInfo.price.diff > 0"
+              src="@/assets/img/balance__procent_img1.png"
+              alt=""
+            />
+            <img v-else src="@/assets/img/balance__procent_img2.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div class="balance-bottom">
+        Balance:
+        {{
+          usdBalance(
+            Number(token.rawBalance),
+            token.tokenInfo.price.rate,
+            token.tokenInfo.decimals
+          )
+        }}
+        $
+        <br />
+        <span>
+          {{
+            Number(token.rawBalance) | normalizeAmount(token.tokenInfo.decimals)
+          }}
+          {{ token.tokenInfo.symbol }}
+        </span>
+      </div>
+    </a>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'nuxt-property-decorator'
+
+@Component({})
+export default class SidebarRightBalance extends Vue {
+  public usdBalance(balance: number, rate: number, decimals: number): string {
+    if (rate > 0) {
+      return ((balance / 10 ** decimals) * rate).toFixed(2)
+    }
+    return String(0)
+  }
+}
+</script>
