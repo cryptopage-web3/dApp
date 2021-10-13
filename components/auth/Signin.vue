9a8531d192d53ca6e4ea5f3b6a3a8d8d06861b8a
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2021-10-13 21:44:44 +0400

    Update Signin component

--- components/auth/Signin.vue
+++ components/auth/Signin.vue
@@ -6,36 +6,14 @@ export default {
   methods: {
     async init() {
       const response = await this.$store.dispatch('auth/signin')
-      if (response.status === 'success') {
-        this.$notify({
-          type: 'success',
-          title: 'Successfully logged in'
-        })
-      } else if (response.message === 'MetaMask not installed') {
-        this.$notify({
-          type: 'info',
-          title:
-            'Please install MetaMask extension for sign in<br>reload page and try again',
-          text: `<div class="notification-content__mt">
-              <a href="https://chrome.google.com/webstore/detail/nkbihfbeogaeaoehlefnkodbefgpgknn" target="_blank">
-                Install MetaMask for Chrome
-              </a>
-            </div>`
-        })
-      } else if (response.message === 'Not authorized in MetaMask') {
-        this.$notify({
-          type: 'info',
-          title:
-            'Please log in to the MetaMask Ext.<br>reload page and try again'
-        })
-      } else {
-        this.$notify({
-          type: 'error',
-          title: 'Something went wrong',
-          text: `<div class="notification-content__mt">
-            Please reload page and try again
-          </div>`
-        })
+      this.$notify({
+        type: response.status,
+        title: response.message.title,
+        text: response.message.text
+      })
+      if (response.status === 'success' && this.$route.path === '/') {
+        const address = await this.$store.getters['auth/selectedAddress']
+        this.$router.push(`/${address}`)
       }
     }
   }
