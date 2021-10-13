63776dfaeee6b47fb7c9eea2fe5962244b354d5b
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2021-10-13 14:19:22 +0400

    Update Signin component

--- components/auth/Signin.vue
+++ components/auth/Signin.vue
@@ -2,57 +2,33 @@
   <div />
 </template>
 <script>
-import { recoverPersonalSignature } from 'eth-sig-util'
-
 export default {
   methods: {
     async init() {
-      try {
-        if (!this.$provider.metamaskInstalled) {
-          this.$notify({
-            type: 'info',
-            title:
-              'Please install MetaMask extension for sign in<br>reload page and try again',
-            text: `<div class="notification-content__mt">
-                <a href="https://chrome.google.com/webstore/detail/nkbihfbeogaeaoehlefnkodbefgpgknn" target="_blank">
-                  Install MetaMask for Chrome
-                </a>
-              </div>`
-          })
-          return
-        }
-
-        if (!this.$provider.metamaskConnected) {
-          this.$notify({
-            type: 'info',
-            title:
-              'Please log in to the MetaMask Ext.<br>reload page and try again'
-          })
-          return
-        }
-
-        const address = this.$provider.selectedAddress
-        // const redirectURL = this.$route.query.next ? this.$route.query.next : ''
-        const signaturePhrase = await this.getSignaturePhrase(address)
-        const sig = await this.$provider.provider.request({
-          method: 'personal_sign',
-          params: [signaturePhrase, address]
+      const response = await this.$store.dispatch('auth/signin')
+      if (response.status === 'success') {
+        this.$notify({
+          type: 'success',
+          title: 'Successfully logged in'
         })
-        const recoveredAddress = recoverPersonalSignature({
-          data: signaturePhrase,
-          sig
+      } else if (response.message === 'MetaMask not installed') {
+        this.$notify({
+          type: 'info',
+          title:
+            'Please install MetaMask extension for sign in<br>reload page and try again',
+          text: `<div class="notification-content__mt">
+              <a href="https://chrome.google.com/webstore/detail/nkbihfbeogaeaoehlefnkodbefgpgknn" target="_blank">
+                Install MetaMask for Chrome
+              </a>
+            </div>`
         })
-
-        if (recoveredAddress.toLowerCase() === address.toLowerCase()) {
-          this.$notify({
-            type: 'success',
-            title: 'Successfully logged in'
-          })
-          this.$store.dispatch('auth/signin', address)
-          await this.$store.dispatch('auth/updateAddressInfo')
-          return
-        }
-
+      } else if (response.message === 'Not authorized in MetaMask') {
+        this.$notify({
+          type: 'info',
+          title:
+            'Please log in to the MetaMask Ext.<br>reload page and try again'
+        })
+      } else {
         this.$notify({
           type: 'error',
           title: 'Something went wrong',
@@ -60,27 +36,7 @@ export default {
             Please reload page and try again
           </div>`
         })
-      } catch (error) {
-        this.$notify({
-          type: 'error',
-          title: 'Something went wrong',
-          text: `<div class="notification-content__mt">
-            Please reload page and try again
-          </div>`
-        })
-        console.error('ERROR in authenticate:', error) // eslint-disable-line no-console
       }
-    },
-    async getSignaturePhrase(address) {
-      const timestamp = new Date().getTime()
-      const random = Math.random().toString(16).substr(2, length)
-      const message = address + timestamp + random
-      const salt = await this.$sea.work(message, null, null, {
-        name: 'SHA-256'
-      })
-      return await this.$sea.work(message, salt, null, {
-        name: 'SHA-256'
-      })
     }
   }
 }
