fff524ffafecda5b118fd4d75a10b615aaff08c2
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2021-10-17 01:31:23 +0400

    Remove unused web3 utils

--- utils/web3.js
+++ utils/web3.js
@@ -12,56 +12,13 @@ export const shortAddress = (
   }
 }
 
-export const toDecimals = (value, decimals = 18) =>
-  Number(value) / 10 ** decimals
-
-export const tokenURItoURI = (uri) => {
-  if (uri.startsWith('ipfs://ipfs/')) {
-    return uri.replace('ipfs://', 'https://ipfs.io/')
-  } else if (uri.startsWith('ipfs://')) {
-    return uri.replace('ipfs://', 'https://ipfs.io/ipfs/')
-  } else if (uri.startsWith('https://') && !uri.includes('herokuapp')) {
-    // uri = uri.replace('https://', '')
-    // return `https://cors-anywhere.herokuapp.com/${uri}` // Temporary hack to prevent CORS
-    return uri
-  } else if (uri.startsWith('https://')) {
-    return uri
+export const normalizeAmount = (exponentialNumber) => {
+  const str = exponentialNumber.toString()
+  if (str.includes('e')) {
+    const exponent = parseInt(str.split('-')[1], 10)
+    const result = Number(exponentialNumber).toFixed(exponent)
+    return result
+  } else {
+    return exponentialNumber
   }
 }
-
-const nftImageDecoder = (data) => {
-  const defaultImage =
-    'https://ih1.redbubble.net/image.485923660.1240/flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg'
-  let image = data.image || data.image_url || data.image_data || defaultImage
-  if (image.startsWith('ipfs')) {
-    image = tokenURItoURI(image)
-  } else if (image.startsWith('<svg')) {
-    const parser = new DOMParser()
-    const element = parser.parseFromString(image, 'image/svg+xml')
-    const imageData = new XMLSerializer().serializeToString(element)
-    image = btoa(imageData)
-    image = `data:image/svg+xml;base64,${image}`
-  }
-  return image
-}
-
-export const nftDataDecoder = (data) => {
-  if (typeof data === 'object') {
-    const title = data.title || data.name
-    const description = data.description || ''
-    const image = nftImageDecoder(data)
-    const owner = data.owner
-    const externalURL = data.external_url || ''
-    return { title, description, image, externalURL, owner }
-  }
-  return null
-}
-
-export const validateTransaction = (tx) => tx && tx.to !== null
-export const isFrom = (address, tx) =>
-  tx.from && address.toLowerCase() === tx.from.toLowerCase()
-export const isTo = (address, tx) =>
-  tx.to && address.toLowerCase() === tx.to.toLowerCase()
-
-export const isOwnerTransaction = (address, tx) =>
-  isFrom(address, tx) || isTo(address, tx)
