99f2d23f5c29c6d274a4efe79c2ef89cbcb95774
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2021-10-17 01:28:32 +0400

    Update filters plugin

--- plugins/filters.js
+++ plugins/filters.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 
 import { normalizeDate, humanizeDate, humanizeCount } from '~/utils/humanize'
-import { shortAddress, toDecimals } from '~/utils/web3'
+import { shortAddress, normalizeAmount } from '~/utils/web3'
 
-Vue.filter('normalizeAmount', (value, decimal) => toDecimals(value, decimal))
-Vue.filter('normalizeDate', (timestamp) => normalizeDate(timestamp))
 Vue.filter('humanizeDate', (value) => humanizeDate(value))
 Vue.filter('humanizeCount', (value) => humanizeCount(value))
+Vue.filter('normalizeDate', (timestamp) => normalizeDate(timestamp))
+Vue.filter('normalizeAmount', (value) => normalizeAmount(value))
 Vue.filter('shortAddress', (address, startCount, endCount, delimiter) =>
   shortAddress(address, startCount, endCount, delimiter)
 )
