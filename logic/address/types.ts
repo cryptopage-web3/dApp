b8d4e5f87d77c64e61617a31af67ee9ce837a88e
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2021-10-17 01:34:16 +0400

    Add address types

--- logic/address/types.ts
+++ logic/address/types.ts
@@ -1,13 +1,34 @@
 import * as ts from 'io-ts'
 import {
   TokenInfo,
+  EthplorerETH,
   EthplorerToken,
   EthplorerTokenInfo,
   EthplorerGetAddressInfoResponse
 } from '~/logic/address/models'
 export type TokenInfoType = ts.TypeOf<typeof TokenInfo>
+export type EthplorerETHType = ts.TypeOf<typeof EthplorerETH>
 export type EthplorerTokenType = ts.TypeOf<typeof EthplorerToken>
 export type EthplorerTokenInfoType = ts.TypeOf<typeof EthplorerTokenInfo>
 export type EthplorerGetAddressInfoResponseType = ts.TypeOf<
   typeof EthplorerGetAddressInfoResponse
 >
+
+export type TokenBalanceType = {
+  balance: number
+  usdBalance: number
+  rate: number
+  diff?: number
+  tokenInfo?: TokenInfoType
+}
+
+export type AddressInfoType = {
+  address: string
+  tokenInfo?: TokenInfoType
+  tokens: TokenBalanceType[]
+  transactionsCount: number
+}
+
+export type AddressInfoAdapterType = {
+  request: () => AddressInfoType
+}
