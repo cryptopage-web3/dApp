6987ed533d38b75ad08a2de0d5cfa6f5c99f48b3
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2021-10-16 20:52:41 +0400

    Add TransactionMixin to PostTop component

--- components/post/PostTop.vue
+++ components/post/PostTop.vue
@@ -4,22 +4,19 @@
       <a href="#" class="post-date__link">
         <img src="@/assets/img/post-date__link_img.png" alt="" />
       </a>
-      <div class="post-date__text">{{ date | normalizeDate }}</div>
+      <div class="post-date__text">
+        {{ transaction.timeStamp | humanizeDate }} ago
+        {{ income ? ' to' : ' from' }} : {{ address | shortAddress }}
+      </div>
     </div>
-    <div class="white-post-id">Txn Hash: {{ hash | shortAddress(5, 7) }}</div>
+    <div class="white-post-id">
+      Txn Hash: {{ transaction.hash | shortAddress(5, 7) }}
+    </div>
   </div>
 </template>
-<script>
-export default {
-  props: {
-    hash: {
-      type: String,
-      required: true
-    },
-    date: {
-      type: String,
-      required: true
-    }
-  }
-}
+<script lang="ts">
+import { Component, mixins } from 'nuxt-property-decorator'
+import TransactionMixin from '~/mixins/transaction'
+@Component({})
+export default class PostTop extends mixins(TransactionMixin) {}
 </script>
