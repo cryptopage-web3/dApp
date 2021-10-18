69b9b0fdc2bc5fc9faa8b609d2534b1a914f461e
Nail Mustyukov <nail.mustyukov@kamtor.com>
2021-10-18 10:52:11 +0500

    nft form: add tooltip to attributes

--- components/nft-form/attributes/NFTFormDates.vue
+++ components/nft-form/attributes/NFTFormDates.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="nft-form__attribute">
     <div class="nft-form__attribute-header" @click="toggle">
-      <div class="nft-form__attribute-header-icon">
+      <div
+        ref="icon"
+        class="nft-form__attribute-header-icon"
+        title="Traits with date"
+      >
         <font-awesome-icon :icon="['fas', 'calendar']" />
       </div>
       <div class="nft-form__attribute-header-title">Dates</div>
@@ -19,6 +23,13 @@ export default {
     return {
       isShow: false
     }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      $(this.$refs.icon).tooltip({
+        trigger: 'hover'
+      })
+    })
   },
   methods: {
     toggle() {
