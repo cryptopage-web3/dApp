802efdd17bd7642373475cb65366369c3e711465
Nail Mustyukov <nail.mustyukov@kamtor.com>
2021-10-26 11:23:35 +0500

    nft form: attributes typescript

--- components/nft-form/attributes/NFTFormAttributes.vue
+++ components/nft-form/attributes/NFTFormAttributes.vue
@@ -11,8 +11,19 @@
     <boosts-attribute :boosts="boosts" @change="boostsChangeHandler" />
   </div>
 </template>
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Emit, Prop, Watch } from 'nuxt-property-decorator'
+import {
+  IAttributeBoost,
+  IAttributeDate,
+  IAttributeLevel,
+  IAttributeProperty,
+  IAttributesFront,
+  IAttributeStat
+} from '../types'
+
+@Component({
   components: {
     'properties-attribute': async () =>
       await import('@/components/nft-form/attributes/NFTFormProperties.vue'),
@@ -26,143 +37,156 @@ export default {
       await import('@/components/nft-form/attributes/NFTFormBoosts.vue'),
     'comment-attribute': async () =>
       await import('@/components/nft-form/attributes/NFTFormComment.vue')
-  },
-  props: {
-    attributes: {
-      type: Object,
-      default: () => ({})
+  }
+})
+export default class NFTFormAttributes extends Vue {
+  isShown = false
+  hasComment = false
+  properties: IAttributeProperty[] = []
+  levels: IAttributeLevel[] = []
+  stats: IAttributeStat[] = []
+  dates: IAttributeDate[] = []
+  boosts: IAttributeBoost[] = []
+
+  $refs!: {
+    attributes: HTMLDivElement
+  }
+
+  @Prop({ type: Object, default: () => ({}) })
+  readonly attributes!: IAttributesFront
+
+  // emit
+
+  @Emit('change')
+  emitChangeAttributes(attributes: IAttributesFront) {
+    return attributes
+  }
+
+  // watch
+
+  @Watch('isShown')
+  onIsShownChanged(isShown: boolean) {
+    if (isShown) {
+      $(this.$refs.attributes).slideDown(200)
+    } else {
+      $(this.$refs.attributes).slideUp(100)
     }
-  },
-  data() {
-    return {
-      isShown: false,
-      hasComment: false,
-      properties: [],
-      levels: [],
-      stats: [],
-      dates: [],
-      boosts: []
+  }
+
+  @Watch('attributes', { immediate: true })
+  onAttributesChanged(attributes: IAttributesFront) {
+    const newProperties = attributes.properties || []
+    const newLevels = attributes.levels || []
+    const newStats = attributes.stats || []
+    const newDates = attributes.dates || []
+    const newBoosts = attributes.boosts || []
+    const hasComment = attributes.hasComment || false
+
+    if (JSON.stringify(newProperties) !== JSON.stringify(this.properties)) {
+      this.properties = newProperties
     }
-  },
-  watch: {
-    isShown(isShown) {
-      if (isShown) {
-        $(this.$refs.attributes).slideDown(200)
-      } else {
-        $(this.$refs.attributes).slideUp(100)
-      }
-    },
 
-    attributes: {
-      handler(attributes) {
-        const newProperties = attributes.properties || []
-        const newLevels = attributes.levels || []
-        const newStats = attributes.stats || []
-        const newDates = attributes.dates || []
-        const newBoosts = attributes.boosts || []
-        const hasComment = attributes.hasComment || false
-
-        if (JSON.stringify(newProperties) !== JSON.stringify(this.properties)) {
-          this.properties = newProperties
-        }
-
-        if (JSON.stringify(newLevels) !== JSON.stringify(this.levels)) {
-          this.levels = newLevels
-        }
-
-        if (JSON.stringify(newStats) !== JSON.stringify(this.stats)) {
-          this.stats = newStats
-        }
-
-        if (JSON.stringify(newDates) !== JSON.stringify(this.dates)) {
-          this.dates = newDates
-        }
-
-        if (JSON.stringify(newBoosts) !== JSON.stringify(this.boosts)) {
-          this.boosts = newBoosts
-        }
-
-        if (hasComment !== this.hasComment) {
-          this.hasComment = hasComment
-        }
-      },
-      immediate: true
-    },
-
-    properties(properties) {
-      this.$emit('change', {
-        ...this.attributes,
-        properties
-      })
-    },
-
-    levels(levels) {
-      this.$emit('change', {
-        ...this.attributes,
-        levels
-      })
-    },
-
-    stats(stats) {
-      this.$emit('change', {
-        ...this.attributes,
-        stats
-      })
-    },
-
-    dates(dates) {
-      this.$emit('change', {
-        ...this.attributes,
-        dates
-      })
-    },
-
-    boosts(boosts) {
-      this.$emit('change', {
-        ...this.attributes,
-        boosts
-      })
-    },
-
-    hasComment(hasComment) {
-      this.$emit('change', {
-        ...this.attributes,
-        hasComment
-      })
+    if (JSON.stringify(newLevels) !== JSON.stringify(this.levels)) {
+      this.levels = newLevels
     }
-  },
-  methods: {
-    toggle() {
-      this.isShown = !this.isShown
-    },
 
-    hide() {
-      this.isShown = false
-    },
-
-    show() {
-      this.isShown = true
-    },
-
-    propertiesChangeHandler(properties) {
-      this.properties = properties
-    },
-
-    levelsChangeHandler(levels) {
-      this.levels = levels
-    },
-
-    statsChangeHandler(stats) {
-      this.stats = stats
-    },
-
-    datesChangeHandler(dates) {
-      this.dates = dates
-    },
-
-    boostsChangeHandler(boosts) {
-      this.boosts = boosts
+    if (JSON.stringify(newStats) !== JSON.stringify(this.stats)) {
+      this.stats = newStats
     }
+
+    if (JSON.stringify(newDates) !== JSON.stringify(this.dates)) {
+      this.dates = newDates
+    }
+
+    if (JSON.stringify(newBoosts) !== JSON.stringify(this.boosts)) {
+      this.boosts = newBoosts
+    }
+
+    if (hasComment !== this.hasComment) {
+      this.hasComment = hasComment
+    }
+  }
+
+  @Watch('properties')
+  onPropertiesChanged(properties: IAttributeProperty[]) {
+    this.emitChangeAttributes({
+      ...this.attributes,
+      properties
+    })
+  }
+
+  @Watch('levels')
+  onLevelsChanged(levels: IAttributeLevel[]) {
+    this.emitChangeAttributes({
+      ...this.attributes,
+      levels
+    })
+  }
+
+  @Watch('stats')
+  onStatsChanged(stats: IAttributeStat[]) {
+    this.emitChangeAttributes({
+      ...this.attributes,
+      stats
+    })
+  }
+
+  @Watch('dates')
+  onDatesChanged(dates: IAttributeDate[]) {
+    this.emitChangeAttributes({
+      ...this.attributes,
+      dates
+    })
+  }
+
+  @Watch('boosts')
+  onBoostsChanged(boosts: IAttributeBoost[]) {
+    this.emitChangeAttributes({
+      ...this.attributes,
+      boosts
+    })
+  }
+
+  @Watch('hasComment')
+  onHasCommentChanged(hasComment: boolean) {
+    this.emitChangeAttributes({
+      ...this.attributes,
+      hasComment
+    })
+  }
+
+  // methods
+
+  toggle() {
+    this.isShown = !this.isShown
+  }
+
+  hide() {
+    this.isShown = false
+  }
+
+  show() {
+    this.isShown = true
+  }
+
+  propertiesChangeHandler(properties: IAttributeProperty[]) {
+    this.properties = properties
+  }
+
+  levelsChangeHandler(levels: IAttributeLevel[]) {
+    this.levels = levels
+  }
+
+  statsChangeHandler(stats: IAttributeStat[]) {
+    this.stats = stats
+  }
+
+  datesChangeHandler(dates: IAttributeDate[]) {
+    this.dates = dates
+  }
+
+  boostsChangeHandler(boosts: IAttributeBoost[]) {
+    this.boosts = boosts
   }
 }
 </script>
