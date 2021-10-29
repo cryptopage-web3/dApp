a9f09b1d763cb5b2497359801ac5a12d2ac07564
Nail Mustyukov <nail.mustyukov@kamtor.com>
2021-10-29 12:45:51 +0500

    address page: copy to clipboard

---
+++ utils/copyToClipboard.ts
@@ -0,0 +1,14 @@
+export const copyToClipboard = (text: string): void => {
+  const tempInput = $('<input>')
+  tempInput.prop('readonly', true)
+  tempInput.css({
+    position: 'absolute',
+    left: '-9999px'
+  })
+
+  $('body').append(tempInput)
+  tempInput.val(text).select()
+  document.execCommand('copy')
+
+  tempInput.remove()
+}
