3dd20f2417cb81518b6d8e63bd687b119ba90cc7
Nail Mustyukov <nail.mustyukov@kamtor.com>
2021-10-31 05:31:39 +0500

    likes: remove ts from likes

--- pages/_networkName/_address/likes.vue
+++ pages/_networkName/_address/likes.vue
@@ -3,8 +3,6 @@
     <div class="transactions-body__empty">No likes</div>
   </div>
 </template>
-<script lang="ts">
-import Vue from 'vue'
-
-export default class extends Vue {}
+<script>
+export default {}
 </script>
