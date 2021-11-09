359903b76a79eada43b41fc23280cc3a33abcbe3
Nail Mustyukov <nail.mustyukov@kamtor.com>
2021-11-10 04:54:14 +0500

    nft: add comment style and some improvement

--- components/post/PostBottom.vue
+++ components/post/PostBottom.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="post-bottom">
     <div class="post-like-dis">
-      <a href="#" class="post-like" @click.prevent="like">
+      <a href="#" class="post-like" @click.prevent="onLike">
         <img src="@/assets/img/post-like_img1.png" alt="" />
         <span> 0 </span>
       </a>
-      <a href="#" class="post-dis" @click.prevent="dislike">
+      <a href="#" class="post-dis" @click.prevent="onDislike">
         <img src="@/assets/img/post-like_img2.png" alt="" />
         <span> 0 </span>
       </a>
@@ -14,11 +14,32 @@
       <form>
         <div class="post-comment-wr">
           <input
+            v-model="comment"
+            :disabled="loading"
             type="text"
             placeholder="Your comment text"
             class="post-comment__input"
           />
-          <button class="post-comment__btn">Send</button>
+          <button
+            :disabled="loading"
+            class="post-comment__btn"
+            @click.prevent="submit"
+          >
+            <span
+              v-if="loading"
+              class="spinner-border spinner-border-sm"
+              role="status"
+              aria-hidden="true"
+            />
+            <template v-else>Send</template>
+          </button>
+          <div
+            v-if="!loading"
+            class="post-comment__close"
+            @click.prevent="close"
+          >
+            <font-awesome-icon :icon="['fas', 'times']" />
+          </div>
         </div>
       </form>
     </div>
@@ -26,18 +47,49 @@
 </template>
 <script>
 export default {
+  data: () => ({
+    loading: false,
+    comment: '',
+    like: true
+  }),
+
   methods: {
-    like(event) {
+    onLike(event) {
+      this.like = true
+
       const targetEl = event.target
       $(targetEl).closest('.post-like-dis').find('.post-dis').fadeOut(0)
       $(targetEl).closest('.post-bottom').find('.post-comment').fadeIn(300)
       $(targetEl).closest('.post-bottom').addClass('active')
     },
 
-    dislike(event) {
+    onDislike(event) {
+      this.like = false
+
       const targetEl = event.target
       $(targetEl).closest('.post-bottom').find('.post-comment').fadeIn(300)
       $(targetEl).closest('.post-bottom').addClass('active')
+    },
+
+    close(event) {
+      const targetEl = event.target
+      $(targetEl).closest('.post-bottom').removeClass('active')
+      $(targetEl).closest('.post-bottom').find('.post-comment').fadeOut(300)
+      $(targetEl).closest('.post-bottom').find('.post-dis').fadeIn(300)
+    },
+
+    submit() {
+      if (!this.comment) {
+        this.$notify({
+          type: 'error',
+          title: 'Empty comment'
+        })
+        return
+      }
+
+      this.loading = true
+      // console.log(this.comment)
+      // debugger
     }
   }
 }
