7b34759493ce06b2f0248929f20c6fa02a9199f8
Gurgen <1gurgenmuradyan@gmail.com>
2022-01-11 20:14:39 +0300

    fix token text

--- constants/tokenNames.ts
+++ constants/tokenNames.ts
@@ -1,7 +1,13 @@
 const tokenNames: Record<string, string> = {
   eth: 'erc20',
+  ropsten: 'erc20',
+  rinkeby: 'erc20',
+  goerly: 'erc20',
+  kovan: 'erc20',
   bsc: 'bep20',
+  'bsc-testnet': 'bep20',
   polygon: 'erc20',
+  'polygon-testnet': 'erc20',
   tron: 'trc20'
 }
 
