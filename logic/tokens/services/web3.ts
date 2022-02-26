8126bc04d0f4b4f0085d49bec8029e89ff850e63
Nail Mustyukov <nail.mustyukov@kamtor.com>
2022-02-27 00:52:59 +0500

    web3: fix get balances from base tokens

--- logic/tokens/services/web3.ts
+++ logic/tokens/services/web3.ts
@@ -1,23 +1,21 @@
 import * as tPromise from 'io-ts-promise'
 import { Service, Inject } from 'vue-typedi'
 import Web3 from 'web3'
-import { ERC20ABI, BalanceCheckerABI } from '~/constants/abi-samples'
+import { ERC20ABI } from '~/constants/abi-samples'
 import { TokenInfo } from '~/logic/tokens/models'
 import { TokenInfoType, TokenBalanceType } from '~/logic/tokens/types'
-import TokenIPFSService from '~/logic/tokens/services/ipfs'
-import AuthService from '~/logic/auth/service'
 import tokens from '~/logic/tokens'
+import AddressService from '~/logic/address/services'
+import { web3Builder } from '~/utils/web3Builder'
 
 @Service(tokens.TOKEN_WEB3_SERVICE)
 export default class TokenWeb3Service {
-  @Inject(tokens.AUTH_SERVICE)
-  public authService!: AuthService
+  @Inject(tokens.ADDRESS_SERVICE)
+  public addressService!: AddressService
 
-  @Inject(tokens.TOKEN_IPFS_SERVICE)
-  public tokenIPFSService!: TokenIPFSService
-
-  @Inject(tokens.WEB3)
-  public $web3!: Web3
+  public get $web3(): Web3 {
+    return web3Builder(this.addressService.chainId)
+  }
 
   /**
    * Get contract name, totalSupply, decimals and symbol from  web3
@@ -31,6 +29,7 @@ export default class TokenWeb3Service {
       const totalSupply = await contract.methods.totalSupply().call()
       const decimals = Number(await contract.methods.decimals().call())
       const symbol = await contract.methods.symbol().call()
+
       return await tPromise.decode(TokenInfo, {
         name,
         address,
@@ -47,55 +46,106 @@ export default class TokenWeb3Service {
     address: string
   ): Promise<TokenBalanceType[]> => {
     try {
-      const tokens = await this.tokenIPFSService.getAllTokens()
-      const contractAddresses: string[] = []
-      tokens.forEach((token, index) => {
-        /**
-         * Wrong addresses exists in the IPFSStorage response
-         * Need to fix it
-         */
-        if (token.address.startsWith('0x')) {
-          contractAddresses[index] = token.address
-        }
-      })
-      const networks: { [chainId: number]: string } = {
-        1: '0xb1f8e55c7f64d203c1400b9d8555d050f94adf39',
-        3: '0x8D9708f3F514206486D7E988533f770a16d074a7',
-        4: '0x3183B673f4816C94BeF53958BaF93C671B7F8Cf2',
-        42: '0x55ABBa8d669D60A10c104CC493ec5ef389EC92bb',
-        56: '0x2352c63A83f9Fd126af8676146721Fa00924d7e4',
-        97: '0x2352c63A83f9Fd126af8676146721Fa00924d7e4',
-        137: '0x2352c63A83f9Fd126af8676146721Fa00924d7e4',
-        80001: '0x2352c63A83f9Fd126af8676146721Fa00924d7e4'
-      }
-      const contractAddress =
-        this.authService.selectedChainId in Object.keys(networks)
-          ? networks[this.authService.selectedChainId]
-          : networks[1]
-      const contract = new this.$web3.eth.Contract(
-        BalanceCheckerABI,
-        contractAddress
+      const balances = await Promise.all(
+        Object.keys(this.baseTokens).map(async (chainId) => {
+          const web3 = web3Builder(chainId)
+          return await web3.eth.getBalance(address)
+        })
       )
-      const balances = await contract.methods
-        .balances([address], contractAddresses)
-        .call()
 
       const result: TokenBalanceType[] = []
-      tokens.forEach((tokenInfo: TokenInfoType, index: number): void => {
-        const balance = balances[index] / 10 ** tokenInfo.decimals
-        tokenInfo.rate = { usd: 0 }
-        if (Number(balance) > 0) {
-          result.push({
-            balance,
-            usdBalance: 0,
-            diff: 0,
-            tokenInfo
-          })
+
+      Object.values(this.baseTokens).forEach(
+        (tokenInfo: TokenInfoType, index: number): void => {
+          const value = Number(balances[index]) || 0
+          const balance = value / 10 ** tokenInfo.decimals
+
+          if (Number(balance) > 0) {
+            result.push({
+              balance,
+              usdBalance: 0,
+              diff: 0,
+              tokenInfo
+            })
+          }
         }
-      })
+      )
+
       return result
     } catch (error) {
       return []
+    }
+  }
+
+  public get baseTokens(): Record<number, any> {
+    const baseURL =
+      'https://www.covalenthq.com/static/images/icons/display-icons/'
+
+    return {
+      1: {
+        address: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+        name: 'Ethereum coin',
+        symbol: 'ETH',
+        decimals: 18,
+        image: `${baseURL}ethereum-eth-logo.png`,
+        rate: { usd: 0 }
+      },
+      3: {
+        address: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+        name: 'Ethereum coin',
+        symbol: 'ETH',
+        decimals: 18,
+        image: `${baseURL}ethereum-eth-logo.png`,
+        rate: { usd: 0 }
+      },
+      4: {
+        address: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+        name: 'Ethereum coin',
+        symbol: 'ETH',
+        decimals: 18,
+        image: `${baseURL}ethereum-eth-logo.png`,
+        rate: { usd: 0 }
+      },
+      42: {
+        address: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+        name: 'Ethereum coin',
+        symbol: 'ETH',
+        decimals: 18,
+        image: `${baseURL}ethereum-eth-logo.png`,
+        rate: { usd: 0 }
+      },
+      56: {
+        address: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+        name: 'Binance coin',
+        symbol: 'BNB',
+        decimals: 18,
+        image: `${baseURL}binance-coin-bnb-logo.png`,
+        rate: { usd: 0 }
+      },
+      97: {
+        address: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+        name: 'Binance coin',
+        symbol: 'BNB',
+        decimals: 18,
+        image: `${baseURL}binance-coin-bnb-logo.png`,
+        rate: { usd: 0 }
+      },
+      137: {
+        address: '0x0000000000000000000000000000000000001010',
+        name: 'Polygon coin',
+        symbol: 'MATIC',
+        decimals: 18,
+        image: `https://logos.covalenthq.com/tokens/1/0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0.png`,
+        rate: { usd: 0 }
+      },
+      80001: {
+        address: '0x0000000000000000000000000000000000001010',
+        name: 'Polygon coin',
+        symbol: 'MATIC',
+        decimals: 18,
+        image: `https://logos.covalenthq.com/tokens/1/0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0.png`,
+        rate: { usd: 0 }
+      }
     }
   }
 }
