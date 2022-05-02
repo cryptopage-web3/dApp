52759ea520d159edb624b52d6d337686512f3e20
Nail Mustyukov <nail.mustyukov@kamtor.com>
2022-05-03 04:08:22 +0500

    address: fix mobile

--- utils/accountLeftSidebar.ts
+++ utils/accountLeftSidebar.ts
@@ -16,4 +16,20 @@ export const accountLeftSidebarInit = (): void => {
       $(this).toggleClass('night-mode-active');
     });
   }
+
+  if ($('.profile-menu__oc').is('.profile-menu__oc')) {
+    $('.profile-menu__oc').on('click', function (event) {
+      event.preventDefault();
+      $('body').addClass('market-main-filtr-col__body');
+      $('.profile-menu-body').slideDown(300);
+    });
+  }
+
+  if ($('.profile-menu-body__close').is('.profile-menu-body__close')) {
+    $('.profile-menu-body__close').on('click', function (event) {
+      event.preventDefault();
+      $('body').removeClass('market-main-filtr-col__body');
+      $('.profile-menu-body').slideUp(300);
+    });
+  }
 };
