cd3cf0b92e86c460a88b0e96798c60c65edd8f0b
Nail Mustyukov <nail.mustyukov@kamtor.com>
2022-06-26 03:00:06 +0500

    add design3 dir

---
+++ _design3/gulpfile.js
@@ -0,0 +1,145 @@
+'use strict';
+
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+prefixer = require('gulp-autoprefixer'),
+uglify = require('gulp-uglify'),
+sass = require('gulp-sass'),
+sourcemaps = require('gulp-sourcemaps'),
+rigger = require('gulp-rigger'),
+cssmin = require('gulp-minify-css'),
+imagemin = require('gulp-imagemin'),
+pngquant = require('imagemin-pngquant'),
+rimraf = require('rimraf'),
+browserSync = require("browser-sync"),
+reload = browserSync.reload;
+
+
+var path = {
+    build: { //Тут мы укажем куда складывать готовые после сборки файлы
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        audio: 'build/audio/',
+        video: 'build/video/',
+        fonts: 'build/fonts/'
+    },
+    src: { //Пути откуда брать исходники
+        html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        js: 'src/js/main.js',//В стилях и скриптах нам понадобятся только main файлы
+        style: 'src/css/main.scss',
+        img: 'src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+        audio: 'src/audio/**/*.*',
+        video: 'src/video/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+        html: 'src/**/*.html',
+        js: 'src/js/**/*.js',
+        style: 'src/css/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    clean: './build'
+};
+
+var config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: false,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Frontend_Devil"
+};
+
+gulp.task('html:build', function () {
+    return gulp.src(path.src.html) //Выберем файлы по нужному пути
+        .pipe(rigger()) //Прогоним через rigger
+        .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
+        .pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
+    });
+
+gulp.task('js:build', function () {
+    return gulp.src(path.src.js) //Найдем наш main файл
+        .pipe(rigger()) //Прогоним через rigger
+        //.pipe(sourcemaps.init()) //Инициализируем sourcemap
+        //.pipe(uglify()) //Сожмем наш js
+        //.pipe(sourcemaps.write()) //Пропишем карты
+        .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
+        .pipe(reload({stream: true})); //И перезагрузим сервер
+    });
+
+gulp.task('style:build', function () {
+    return gulp.src(path.src.style) //Выберем наш main.scss
+        //.pipe(sourcemaps.init()) //То же самое что и с js
+        .pipe(sass()) //Скомпилируем
+        .pipe(prefixer()) //Добавим вендорные префиксы
+        .pipe(cssmin()) //Сожмем
+        //.pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css)) //И в build
+        .pipe(reload({stream: true}));
+    });
+
+gulp.task('image:build', function () {
+    return gulp.src(path.src.img) //Выберем наши картинки
+    .pipe(imagemin([
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.mozjpeg({quality: 98, progressive: false}),
+        //imagemin.optipng({optimizationLevel: 5}),
+        /*imagemin.svgo({
+            plugins: [
+            {removeViewBox: false},
+            {cleanupIDs: false}
+            ]
+        })*/
+        ]))
+        .pipe(gulp.dest(path.build.img)) //И бросим в build
+        .pipe(reload({stream: true}));
+    });
+
+gulp.task('audio:build', function () {
+    return gulp.src(path.src.audio) //Выберем наши картинки
+        .pipe(gulp.dest(path.build.audio)) //И бросим в build
+        .pipe(reload({stream: true}));
+    });
+
+gulp.task('video:build', function () {
+    return gulp.src(path.src.video) //Выберем наши картинки
+        .pipe(gulp.dest(path.build.video)) //И бросим в build
+        .pipe(reload({stream: true}));
+    });
+
+gulp.task('fonts:build', function() {
+    return gulp.src(path.src.fonts)
+    .pipe(gulp.dest(path.build.fonts));
+});
+
+gulp.task('build', gulp.parallel(
+    'html:build',
+    'js:build',
+    'style:build',
+    'fonts:build',
+    'image:build',
+    'audio:build',
+    'video:build',
+    ));
+
+gulp.task('watch', function(){
+    gulp.watch([path.watch.html], gulp.parallel('html:build'));
+    gulp.watch([path.watch.style], gulp.parallel('style:build'));
+    gulp.watch([path.watch.js], gulp.parallel('js:build'));
+    gulp.watch([path.watch.img], gulp.parallel('image:build'));
+    gulp.watch([path.watch.fonts], gulp.parallel('fonts:build'));
+});
+
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+gulp.task('default', gulp.parallel('build', 'webserver', 'watch'));
