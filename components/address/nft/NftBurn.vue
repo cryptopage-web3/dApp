001e5c29831b676fbcf56633bb02141504fb3625
Nail Mustyukov <nail.mustyukov@kamtor.com>
2022-06-28 19:01:20 +0500

    nft delete: show confirm modal

--- components/address/nft/NftBurn.vue
+++ components/address/nft/NftBurn.vue
@@ -6,9 +6,10 @@
       </a>
     </li>
     <li>
-      <a href="#">
+      <a href="#" @click.prevent="showModal">
         <NftBurnIcon />
       </a>
+      <NftDeleteConfirm ref="modal" />
     </li>
   </ul>
 </template>
@@ -19,6 +20,7 @@ import { Component, Prop } from 'nuxt-property-decorator';
 import { INft } from '~/types';
 import NftShareIcon from '~/components/icon/nft/NftShareIcon.vue';
 import NftBurnIcon from '~/components/icon/nft/NftBurnIcon.vue';
+import NftDeleteConfirm from '~/components/address/nft/NftDeleteConfirm.vue';
 
 type TNft = INft;
 
@@ -26,10 +28,19 @@ type TNft = INft;
   components: {
     NftShareIcon,
     NftBurnIcon,
+    NftDeleteConfirm,
   },
 })
 export default class NftBurn extends Vue {
   @Prop({ required: true })
   readonly nft!: TNft;
+
+  $refs!: {
+    modal: NftDeleteConfirm;
+  };
+
+  showModal() {
+    this.$refs.modal.show();
+  }
 }
 </script>
---
+++ components/address/nft/NftDeleteConfirm.vue
@@ -0,0 +1,74 @@
+<template>
+  <div
+    ref="modal"
+    class="modal fade modal-celar-history"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-body">
+          <div class="modal-top mb_20">
+            <div class="modal-creat__title">Delete post</div>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <CommentCloseIcon />
+            </button>
+          </div>
+          <div class="modal-celar-history__text">Are you sure?</div>
+          <div class="modal-celar-history-btns mt_20">
+            <a
+              href="#"
+              role="button"
+              data-dismiss="modal"
+              class="btn btn-blue-transparent_button btn_large w_110 mr_20"
+            >
+              No
+            </a>
+            <a
+              href="#"
+              role="button"
+              data-dismiss="modal"
+              class="btn btn-blue_button btn_large w_110"
+            >
+              Yes
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component, Prop } from 'nuxt-property-decorator';
+import { INft } from '~/types';
+import CommentCloseIcon from '~/components/icon/nft/CommentCloseIcon.vue';
+
+type TNft = INft;
+
+@Component({
+  components: {
+    CommentCloseIcon,
+  },
+})
+export default class NftDeleteConfirm extends Vue {
+  @Prop({ required: true })
+  readonly nft!: TNft;
+
+  $refs!: {
+    modal: HTMLDivElement;
+  };
+
+  show() {
+    ($(this.$refs.modal) as any).modal('show');
+  }
+}
+</script>
