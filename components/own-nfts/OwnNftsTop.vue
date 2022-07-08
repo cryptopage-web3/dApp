983fd4125158a5665421fce328dd2a241fca99d3
Nail Mustyukov <nail.mustyukov@kamtor.com>
2022-07-08 17:04:31 +0500

    my nfts: components decomposition, load data

---
+++ components/own-nfts/OwnNftsTop.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="profile-my-nfts-top">
+    <div class="profile-my-nfts__title">
+      <nuxt-link :to="`/${chainSlug}/${address}`" class="market-sidebar__more">
+        <OwnNftsBackIcon />
+      </nuxt-link>
+      <h2 class="global-zag">{{ isOwner ? 'My' : 'Account' }} NFT’s</h2>
+    </div>
+    <div class="profile-my-nfts-top__btns">
+      <a href="#" class="btn-blue-transparent"> + Create NFT </a>
+      <a
+        href="#"
+        class="btn_transparent"
+        role="button"
+        data-toggle="modal"
+        data-target="#profile-nfts-board-modal"
+      >
+        + Board
+      </a>
+    </div>
+    <!-- TODO: модалка добавления Board -->
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'nuxt-property-decorator';
+import { addressModule, authModule } from '~/store';
+import OwnNftsBackIcon from '~/components/icon/own-nfts/OwnNftsBackIcon.vue';
+
+@Component({
+  components: {
+    OwnNftsBackIcon,
+  },
+})
+export default class OwnNftsTop extends Vue {
+  get chainSlug(): string {
+    return addressModule.chainSlug;
+  }
+
+  get address(): string {
+    return addressModule.address;
+  }
+
+  get isOwner(): boolean {
+    return (
+      authModule.address.toLowerCase() ===
+        addressModule.address.toLowerCase() &&
+      authModule.chainSlug === addressModule.chainSlug
+    );
+  }
+}
+</script>
