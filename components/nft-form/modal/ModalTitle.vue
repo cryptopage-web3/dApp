b40e64fa57515d90cfd2c4fb4b080632dad2cd99
Nail Mustyukov <nail.mustyukov@kamtor.com>
2022-07-28 15:27:04 +0500

    nft modal: sync title and description

---
+++ components/nft-form/modal/ModalTitle.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="form-field mb_20">
+    <div class="form-field__title">Name</div>
+    <input
+      type="text"
+      :value="title"
+      class="global-input global-input_large modal-creat-input-js"
+      placeholder="Enter name item"
+      @input="updateTitle"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'nuxt-property-decorator';
+import { nftFormModule } from '~/store';
+
+@Component({})
+export default class ModalTitle extends Vue {
+  get title(): string {
+    return nftFormModule.values.title;
+  }
+
+  updateTitle(e: InputEvent) {
+    nftFormModule.setTitle((e.target as HTMLInputElement)?.value || '');
+  }
+}
+</script>
