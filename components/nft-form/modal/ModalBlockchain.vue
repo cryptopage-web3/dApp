f7c44bc986ee5d74f74f2a274eb39977b703bf37
Nail Mustyukov <nail.mustyukov@kamtor.com>
2022-08-14 10:09:34 +0300

    nft form: fix blockchain field

--- components/nft-form/modal/ModalBlockchain.vue
+++ components/nft-form/modal/ModalBlockchain.vue
@@ -1,68 +1,27 @@
 <template>
   <div class="modal-creat-text mb_20">
     <div class="modal-creat__title2">Blockchain</div>
-    <div class="profile-content-drop modal-creat-drop2 market-filtr__drop mt_5">
+    <div
+      ref="drop"
+      class="profile-content-drop modal-creat-drop2 market-filtr__drop mt_5"
+    >
       <a href="#" class="profile-content-drop__link">
-        <div class="thumb">
-          <img src="@/assets/img/market-header__wallet_icon2.svg" alt="" />
-        </div>
-        <span> Ethereum </span>
+        <template v-if="chainInfo">
+          <div class="thumb">
+            <img :src="chainInfo.icon" alt="" />
+          </div>
+          <span> {{ chainInfo.title }} </span>
+        </template>
+        <span v-else> Select value </span>
       </a>
       <div class="drop-down__col">
         <ul class="drop-down__list">
-          <li>
-            <a href="#">
+          <li v-for="item in chainList" :key="item.slug">
+            <a href="#" @click.prevent="select(item.slug)">
               <div class="thumb">
-                <img
-                  src="@/assets/img/market-header__wallet_icon2.svg"
-                  alt=""
-                />
+                <img :src="item.icon" alt="" />
               </div>
-              <span> Ethereum </span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <div class="thumb">
-                <img
-                  src="@/assets/img/market-header__wallet_icon3.svg"
-                  alt=""
-                />
-              </div>
-              <span> Binance Smart Chain </span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <div class="thumb">
-                <img
-                  src="@/assets/img/market-header__wallet_icon4.svg"
-                  alt=""
-                />
-              </div>
-              <span> Polygon </span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <div class="thumb">
-                <img
-                  src="@/assets/img/market-header__wallet_icon5.svg"
-                  alt=""
-                />
-              </div>
-              <span> Tron </span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <div class="thumb">
-                <img
-                  src="@/assets/img/market-header__wallet_icon6.svg"
-                  alt=""
-                />
-              </div>
-              <span> Solana </span>
+              <span> {{ item.title }} </span>
             </a>
           </li>
         </ul>
@@ -73,17 +32,71 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import { Component } from 'nuxt-property-decorator';
-import { nftFormModule } from '~/store';
+import { Component, Watch } from 'nuxt-property-decorator';
+import { nftFormModule, authModule } from '~/store';
+import { EChainSlug } from '~/types';
 
 @Component({})
 export default class ModalBlockchain extends Vue {
+  chainList = [
+    {
+      slug: EChainSlug.eth,
+      title: 'Ethereum',
+      icon: require('@/assets/img/market-header__wallet_icon2.svg'),
+    },
+    {
+      slug: EChainSlug.bsc,
+      title: 'Binance Smart Chain',
+      icon: require('@/assets/img/market-header__wallet_icon3.svg'),
+    },
+    {
+      slug: EChainSlug.polygon,
+      title: 'Polygon',
+      icon: require('@/assets/img/market-header__wallet_icon4.svg'),
+    },
+    {
+      slug: EChainSlug.tron,
+      title: 'Tron',
+      icon: require('@/assets/img/market-header__wallet_icon5.svg'),
+    },
+    {
+      slug: EChainSlug.solana,
+      title: 'Solana',
+      icon: require('@/assets/img/market-header__wallet_icon6.svg'),
+    },
+  ];
+
+  $refs!: {
+    drop: HTMLDivElement;
+  };
+
   get chain(): string {
     return nftFormModule.values.chain;
   }
 
-  updateChain(e: InputEvent) {
-    nftFormModule.setChain((e.target as HTMLInputElement)?.value || '');
+  get chainInfo() {
+    return this.chainList.find(({ slug }) => slug === this.chain);
+  }
+
+  get authChainSlug(): string {
+    return authModule.chainSlug;
+  }
+
+  @Watch('authChainSlug', { immediate: true })
+  onAuthChainSlugChanged(slug: string) {
+    if (process.client && slug !== this.chain) {
+      nftFormModule.setChain(slug);
+    }
+  }
+
+  select(slug: EChainSlug) {
+    nftFormModule.setChain(slug);
+    this.hideDrop();
+  }
+
+  hideDrop() {
+    $(this.$refs.drop).find('.drop-down__col').slideUp(300);
+    $(this.$refs.drop).removeClass('active');
   }
 }
 </script>
