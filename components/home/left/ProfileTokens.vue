1c8bec339d5f401e32c1f5630bb25bfb08c87349
Nail Mustyukov <nail.mustyukov@kamtor.com>
2022-11-11 10:19:27 +0500

    home: show tokens balance

--- components/home/left/ProfileTokens.vue
+++ components/home/left/ProfileTokens.vue
@@ -1,23 +1,16 @@
 <template>
   <div class="main-profile-wallet-cont">
     <table class="main-profile-wallet-table">
-      <tr>
+      <tr v-for="token in tokens" :key="token.address">
         <td>
           <div>
-            <img src="@/assets/img/main-profile-wallet-icon1.svg" alt="" />
-            <span> Your ETH: </span>
+            <img :src="token.logo" alt="" />
+            <span> Your {{ token.symbol }}: </span>
           </div>
         </td>
-        <td>21.678345</td>
-      </tr>
-      <tr>
-        <td>
-          <div>
-            <img src="@/assets/img/main-profile-wallet-icon2.svg" alt="" />
-            <span> Your PAGE: </span>
-          </div>
+        <td :title="token.balance">
+          {{ token.balance | formatNumberFloatDigits }}
         </td>
-        <td>213.678341</td>
       </tr>
     </table>
   </div>
@@ -26,7 +19,13 @@
 <script lang="ts">
 import Vue from 'vue';
 import { Component } from 'nuxt-property-decorator';
+import { authModule } from '~/store';
+import { IToken } from '~/types';
 
 @Component({})
-export default class ProfileTokens extends Vue {}
+export default class ProfileTokens extends Vue {
+  get tokens(): IToken[] {
+    return authModule.tokens;
+  }
+}
 </script>
