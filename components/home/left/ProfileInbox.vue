8e5a7002c96c62715c6d0ecf14b129bcbde37b19
Nail Mustyukov <nail.mustyukov@kamtor.com>
2023-01-05 12:23:26 +0500

    home: iframe chat list

--- components/home/left/ProfileInbox.vue
+++ components/home/left/ProfileInbox.vue
@@ -3,110 +3,26 @@
     <div class="main-profile-inbox-top">
       <h3 class="market-sidebar__title">Inbox</h3>
     </div>
-    <a href="#" class="main-profile-inbox-item online pt-0">
-      <div
-        class="chat-list-item-thumb"
-        :style="{
-          backgroundImage: `url(${chatThumb1})`,
-        }"
-      ></div>
-      <div class="chat-list-item-right">
-        <div class="d-flex align-items-start justify-content-between mb_2">
-          <div class="chat-list-item__nik">
-            <span>0xfB9...687</span> / Bernard Show
-          </div>
-        </div>
-        <div class="chat-list-item__text">
-          Added the creation of a group chat, the principle is...
-        </div>
-      </div>
-    </a>
-    <a href="#" class="main-profile-inbox-item">
-      <div
-        class="chat-list-item-thumb"
-        :style="{
-          backgroundImage: `url(${chatThumb2})`,
-        }"
-      ></div>
-      <div class="chat-list-item-right">
-        <div class="d-flex align-items-start justify-content-between mb_2">
-          <div class="chat-list-item__nik">Punk Brew chat company</div>
-        </div>
-        <div class="chat-list-item__text">
-          Added the creation of a group chat, the principle is...
-        </div>
-      </div>
-    </a>
-    <a href="#" class="main-profile-inbox-item">
-      <div
-        class="chat-list-item-thumb"
-        :style="{
-          backgroundImage: `url(${chatThumb3})`,
-        }"
-      ></div>
-      <div class="chat-list-item-right">
-        <div class="d-flex align-items-start justify-content-between mb_2">
-          <div class="chat-list-item__nik">
-            <span>0xf3D...667</span> / Katerine Laskala
-          </div>
-        </div>
-        <div class="chat-list-item__text">
-          Added the creation of a group chat, the principle is...
-        </div>
-      </div>
-    </a>
-    <a href="#" class="main-profile-inbox-item online">
-      <div
-        class="chat-list-item-thumb"
-        :style="{
-          backgroundImage: `url(${chatThumb4})`,
-        }"
-      ></div>
-      <div class="chat-list-item-right">
-        <div class="d-flex align-items-start justify-content-between mb_2">
-          <div class="chat-list-item__nik">
-            <span>0xf3D...667</span> / Katerine Laskala
-          </div>
-        </div>
-        <div class="chat-list-item__text">
-          Added the creation of a group chat, the principle is...
-        </div>
-      </div>
-    </a>
-    <a href="#" class="main-profile-inbox-item online">
-      <div
-        class="chat-list-item-thumb"
-        :style="{
-          backgroundImage: `url(${chatThumb5})`,
-        }"
-      ></div>
-      <div class="chat-list-item-right">
-        <div class="d-flex align-items-start justify-content-between mb_2">
-          <div class="chat-list-item__nik">Fuck the police</div>
-        </div>
-        <div class="chat-list-item__text">
-          Added the creation of a group chat, the principle is...
-        </div>
-      </div>
-    </a>
+    <div class="main-profile-inbox-chats">
+      <iframe
+        :src="iframeUrl"
+        allowtransparency
+        loading="lazy"
+        frameborder="0"
+        class="main-profile-inbox-chats__iframe"
+        sandbox="allow-same-origin || allow-top-navigation || allow-forms || allow-scripts"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import { Component } from 'nuxt-property-decorator';
-import chatThumb1 from '~/assets/img/chat-list-item-thumb1.png';
-import chatThumb2 from '~/assets/img/chat-list-item-thumb2.png';
-import chatThumb3 from '~/assets/img/chat-list-item-thumb3.png';
-import chatThumb4 from '~/assets/img/chat-list-item-thumb4.png';
-import chatThumb5 from '~/assets/img/chat-list-item-thumb5.png';
+import { MESSENGER_CHAT_LIST_SIDEBAR_URL } from '~/constants';
 
 @Component({})
 export default class ProfileInbox extends Vue {
-  chatThumb1 = chatThumb1;
-  chatThumb2 = chatThumb2;
-  chatThumb3 = chatThumb3;
-  chatThumb4 = chatThumb4;
-  chatThumb5 = chatThumb5;
+  iframeUrl = MESSENGER_CHAT_LIST_SIDEBAR_URL;
 }
 </script>
--- constants/index.ts
+++ constants/index.ts
@@ -39,3 +39,7 @@ export const FRACTAL_CLIENT_ID = 'c0uqfU0-sbr4eZkdvr3XXipoVPH91_nqdnCRDkX0OcI';
 export const FRACTAL_LEVEL = 'basic+liveness';
 export const FRACTAL_URL = 'https://credentials.fractal.id';
 export const FRACTAL_VERIFICATION_URL = `https://app.fractal.id/authorize?client_id=${FRACTAL_CLIENT_ID}&redirect_uri=https%3A%2F%2Fapp.crypto.page%2F&response_type=code&scope=contact%3Aread%20verification.basic%3Aread%20verification.basic.details%3Aread%20verification.liveness%3Aread%20verification.liveness.details%3Aread%20verification.wallet-eth%3Aread%20verification.wallet-eth.details%3Aread`;
+
+/** messenger */
+export const MESSENGER_CHAT_LIST_SIDEBAR_URL =
+  'https://inbox.crypto.page/ChatListSidebar';
