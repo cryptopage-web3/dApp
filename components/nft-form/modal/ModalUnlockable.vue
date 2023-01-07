f77a6c4cf3453712bcaf63c290bb2ffacd0fc99c
Nail Mustyukov <nail.mustyukov@kamtor.com>
2023-01-07 08:35:54 +0500

    nft form: change design unlockable content

--- components/nft-form/modal/ModalUnlockable.vue
+++ components/nft-form/modal/ModalUnlockable.vue
@@ -26,18 +26,20 @@
         </label>
       </div>
     </div>
-    <div v-if="isUnlockable" class="form-field modal-creat-text-content mb_20">
-      <div class="">
+    <div
+      v-show="isUnlockable"
+      class="modal-creat-unlocks-tabs voite-collapse mb_20 checkbox-collapse"
+    >
+      <div class="voite-collapse-links-wrap">
         <a
           href="#"
-          class="btn btn_default"
-          :class="
-            unlockableContentAccessType ===
-            ENftFormUnlockableContentAccessType.oneTime
-              ? 'btn-blue_button'
-              : 'btn_grey'
-          "
-          @click="
+          class="btn btn_default voite-collapse-link"
+          :class="{
+            active:
+              unlockableContentAccessType ===
+              ENftFormUnlockableContentAccessType.oneTime,
+          }"
+          @click.prevent="
             nftFormModule.setUnlockableContentAccessType(
               ENftFormUnlockableContentAccessType.oneTime,
             )
@@ -47,14 +49,13 @@
         </a>
         <a
           href="#"
-          class="btn btn_default"
-          :class="
-            unlockableContentAccessType ===
-            ENftFormUnlockableContentAccessType.customDuration
-              ? 'btn-blue_button'
-              : 'btn_grey'
-          "
-          @click="
+          class="btn btn_default voite-collapse-link"
+          :class="{
+            active:
+              unlockableContentAccessType ===
+              ENftFormUnlockableContentAccessType.customDuration,
+          }"
+          @click.prevent="
             nftFormModule.setUnlockableContentAccessType(
               ENftFormUnlockableContentAccessType.customDuration,
             )
@@ -64,40 +65,147 @@
         </a>
       </div>
 
-      <div
-        v-if="
-          unlockableContentAccessType ===
-          ENftFormUnlockableContentAccessType.oneTime
-        "
-      ></div>
-      <div
-        v-else-if="
-          unlockableContentAccessType ===
-          ENftFormUnlockableContentAccessType.customDuration
-        "
-      >
-        <input
-          v-model.number="unlockableContentAccessDuration"
-          type="number"
-          class="global-input global-input_large mt_10"
-          placeholder="Enter access duration days count"
-        />
+      <div class="voite-collapse-wrap">
+        <div
+          v-show="
+            unlockableContentAccessType ===
+            ENftFormUnlockableContentAccessType.oneTime
+          "
+          class="voite-collapse-cont"
+          style="display: block"
+        >
+          <div class="voite-collapse-body">
+            <div class="global-text_12 mb_10">
+              Enter the amount that the user will have to pay to view the
+              content
+            </div>
+            <div class="form-field">
+              <div class="form-field-with-drop">
+                <input
+                  v-model="unlockableContentPrice"
+                  class="global-input global-input_large"
+                  placeholder="Enter content price"
+                />
+                <div
+                  class="profile-content-drop modal-creat-drop market-filtr__drop"
+                >
+                  <a
+                    ref="onetimePriceDropLink"
+                    href="#"
+                    class="form-field-with-drop__link profile-content-drop__link"
+                  >
+                    <img
+                      src="@/assets/img/modal-creat-unlocks-tabs-icon1.svg"
+                      alt=""
+                    />
+                  </a>
+                  <div class="drop-down__col">
+                    <ul class="drop-down__list">
+                      <li>
+                        <a href="#">
+                          <img
+                            src="@/assets/img/modal-creat-unlocks-tabs-icon1.svg"
+                            alt=""
+                          />
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          v-show="
+            unlockableContentAccessType ===
+            ENftFormUnlockableContentAccessType.customDuration
+          "
+          class="voite-collapse-cont"
+          style="display: block"
+        >
+          <div class="voite-collapse-body">
+            <div class="global-text_12 mb_10">
+              This post will be paid as part of the payment for the entire
+              subscription
+            </div>
+            <div class="row modal-creat-unlocks-tabs-row">
+              <div class="col-sm-6 col-12 mb_10 mb_sm_0">
+                <div class="form-field">
+                  <div class="form-field-with-drop form-field-with-drop_text">
+                    <input
+                      v-model="unlockableContentAccessDuration"
+                      class="global-input global-input_large"
+                      placeholder="Enter access duration"
+                    />
+                    <div
+                      class="profile-content-drop modal-creat-drop market-filtr__drop"
+                    >
+                      <a
+                        ref="durationDropLink"
+                        href="#"
+                        class="form-field-with-drop__link profile-content-drop__link"
+                      >
+                        Days
+                      </a>
+                      <div class="drop-down__col">
+                        <ul class="drop-down__list">
+                          <li>
+                            <a href="#"> Days </a>
+                          </li>
+                          <li>
+                            <a href="#"> Weeks </a>
+                          </li>
+                          <li>
+                            <a href="#"> Months </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6 col-12">
+                <div class="form-field">
+                  <div class="form-field-with-drop">
+                    <input
+                      v-model="unlockableContentPrice"
+                      class="global-input global-input_large"
+                      placeholder="Enter content price"
+                    />
+                    <div
+                      class="profile-content-drop modal-creat-drop market-filtr__drop"
+                    >
+                      <a
+                        ref="timePriceDropLink"
+                        href="#"
+                        class="form-field-with-drop__link profile-content-drop__link"
+                      >
+                        <img
+                          src="@/assets/img/modal-creat-unlocks-tabs-icon1.svg"
+                          alt=""
+                        />
+                      </a>
+                      <div class="drop-down__col">
+                        <ul class="drop-down__list">
+                          <li>
+                            <a href="#">
+                              <img
+                                src="@/assets/img/modal-creat-unlocks-tabs-icon1.svg"
+                                alt=""
+                              />
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <input
-        v-model.number="unlockableContentPrice"
-        type="number"
-        class="global-input global-input_large mt_10"
-        placeholder="Enter content price..."
-      />
-
-      <!--      <div class="form-field__title">Text content</div>-->
-      <!--      <textarea-->
-      <!--        class="global-input global-input_textarea"-->
-      <!--        placeholder="Enter text"-->
-      <!--        :value="text"-->
-      <!--        @input="updateText"-->
-      <!--      />-->
     </div>
   </div>
 </template>
@@ -105,16 +213,24 @@
 <script lang="ts">
 import Vue from 'vue';
 import { Component } from 'nuxt-property-decorator';
+import { nftFormModule } from '~/store';
 import {
   ENftFormUnlockableContentAccessType,
   INftForm,
-} from '../../../types/nft-form';
-import { nftFormModule } from '~/store';
+} from '~/types/nft-form';
+import { profileContentDropTarget } from '~/utils/profileContentDrop';
 
 @Component({})
 export default class ModalUnlockable extends Vue {
   ENftFormUnlockableContentAccessType = ENftFormUnlockableContentAccessType;
   nftFormModule = nftFormModule;
+
+  $refs!: {
+    onetimePriceDropLink: HTMLDivElement;
+    durationDropLink: HTMLDivElement;
+    timePriceDropLink: HTMLDivElement;
+  };
+
   get isUnlockable(): boolean {
     return nftFormModule.values.isUnlockableContent;
   }
@@ -129,17 +245,20 @@ export default class ModalUnlockable extends Vue {
     return nftFormModule.values.unlockableContentAccessType;
   }
 
-  get unlockableContentPrice(): INftForm['unlockableContentPrice'] {
-    return nftFormModule.values.unlockableContentPrice / 10 ** 18;
+  get unlockableContentPrice(): string {
+    return String(
+      (nftFormModule.values.unlockableContentPrice || 0) / 10 ** 18,
+    );
   }
 
-  set unlockableContentPrice(v) {
+  set unlockableContentPrice(v: string) {
     nftFormModule.setUnlockableContentPrice(parseInt(v) * 10 ** 18);
   }
 
-  get unlockableContentAccessDuration(): INftForm['unlockableContentAccessDuration'] {
-    return (
-      nftFormModule.values.unlockableContentAccessDuration / (24 * 60 * 60)
+  get unlockableContentAccessDuration(): string {
+    return String(
+      (nftFormModule.values.unlockableContentAccessDuration || 0) /
+        (24 * 60 * 60),
     );
   }
 
@@ -147,6 +266,14 @@ export default class ModalUnlockable extends Vue {
     nftFormModule.setUnlockableContentAccessDuration(
       parseInt(e) * (24 * 60 * 60),
     );
+  }
+
+  mounted() {
+    this.$nextTick(() => {
+      profileContentDropTarget(this.$refs.onetimePriceDropLink);
+      profileContentDropTarget(this.$refs.durationDropLink);
+      profileContentDropTarget(this.$refs.timePriceDropLink);
+    });
   }
 }
 </script>
