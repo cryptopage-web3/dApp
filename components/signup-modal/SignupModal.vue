7f68a01fb8465cbd6bdd165c67dbd479fe9dc463
Nail Mustyukov <nail.mustyukov@kamtor.com>
2023-01-14 11:32:43 +0500

    signup modal: confirm verify step

--- components/signup-modal/SignupModal.vue
+++ components/signup-modal/SignupModal.vue
@@ -1,190 +1,202 @@
 <template>
-  <div
-    id="onboarding-modal-between"
-    ref="modal"
-    class="modal fade global-modal onboarding-modal"
-    tabindex="-1"
-    role="dialog"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-body">
-          <div class="global-modal-body">
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-label="Close"
-            >
-              <SignupModalCloseIcon />
-            </button>
-            <div class="modal-creat__title mb_20">
-              Sign Up • Sign Wallet Request
-            </div>
-            <div class="onboarding-between-steps mb_20">
-              <div
-                class="onboarding-between-step"
-                :class="{ checked: stepIndex > 0, active: stepIndex === 0 }"
+  <div>
+    <div
+      id="onboarding-modal-between"
+      ref="modal"
+      class="modal fade global-modal onboarding-modal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-body">
+            <div class="global-modal-body">
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
               >
-                <span>1</span>
-                <img
-                  src="@/assets/img/onboarding-between-step_img.svg"
-                  alt=""
-                />
+                <SignupModalCloseIcon />
+              </button>
+              <div class="modal-creat__title mb_20">
+                Sign Up • Sign Wallet Request
               </div>
-              <div
-                class="onboarding-between-step"
-                :class="{ checked: stepIndex > 1, active: stepIndex === 1 }"
-              >
-                <span>2</span>
-                <img
-                  src="@/assets/img/onboarding-between-step_img.svg"
-                  alt=""
-                />
-              </div>
-              <div
-                class="onboarding-between-step"
-                :class="{ checked: stepIndex > 2, active: stepIndex === 2 }"
-              >
-                <span>3</span>
-                <img
-                  src="@/assets/img/onboarding-between-step_img.svg"
-                  alt=""
-                />
-              </div>
-              <div
-                class="onboarding-between-step"
-                :class="{ checked: stepIndex > 3, active: stepIndex === 3 }"
-              >
-                <span>4</span>
-                <img
-                  src="@/assets/img/onboarding-between-step_img.svg"
-                  alt=""
-                />
-              </div>
-            </div>
-            <div class="global-line mb_20"></div>
-            <div class="global-text_14 main_black">
-              <p>
-                <strong class="fw-600"
-                  >As part of your sign-up process, you’ll be requared to select
-                  the account to connect and sign three transactions in your
-                  wallet.</strong
+              <div class="onboarding-between-steps mb_20">
+                <div
+                  class="onboarding-between-step"
+                  :class="{ checked: stepIndex > 0, active: stepIndex === 0 }"
                 >
-                We’ll explain why we need each along the way.
-              </p>
-              <p>
-                <strong class="fw-600">Don’t worry, signing up is free.</strong>
-                Additionaly, we will not be privy to your wallet password,
-                private key, balance, or any other information.
-              </p>
+                  <span>1</span>
+                  <img
+                    src="@/assets/img/onboarding-between-step_img.svg"
+                    alt=""
+                  />
+                </div>
+                <div
+                  class="onboarding-between-step"
+                  :class="{ checked: stepIndex > 1, active: stepIndex === 1 }"
+                >
+                  <span>2</span>
+                  <img
+                    src="@/assets/img/onboarding-between-step_img.svg"
+                    alt=""
+                  />
+                </div>
+                <div
+                  class="onboarding-between-step"
+                  :class="{ checked: stepIndex > 2, active: stepIndex === 2 }"
+                >
+                  <span>3</span>
+                  <img
+                    src="@/assets/img/onboarding-between-step_img.svg"
+                    alt=""
+                  />
+                </div>
+                <div
+                  class="onboarding-between-step"
+                  :class="{ checked: stepIndex > 3, active: stepIndex === 3 }"
+                >
+                  <span>4</span>
+                  <img
+                    src="@/assets/img/onboarding-between-step_img.svg"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div class="global-line mb_20"></div>
+              <div class="global-text_14 main_black">
+                <p>
+                  <strong class="fw-600"
+                    >As part of your sign-up process, you’ll be requared to
+                    select the account to connect and sign three transactions in
+                    your wallet.</strong
+                  >
+                  We’ll explain why we need each along the way.
+                </p>
+                <p>
+                  <strong class="fw-600"
+                    >Don’t worry, signing up is free.</strong
+                  >
+                  Additionaly, we will not be privy to your wallet password,
+                  private key, balance, or any other information.
+                </p>
+              </div>
+              <ul class="onboarding-between-list mt_20">
+                <li>
+                  <div class="onboarding-between-list-i">
+                    <img
+                      :src="getStepIcon(ESignupStep.connect)"
+                      alt=""
+                      :class="{
+                        'onboarding-between-list-i__img':
+                          stepIndex === 0 && loading,
+                      }"
+                    />
+                  </div>
+                  <div class="onboarding-between-list-t">
+                    <div
+                      class="global-text_14"
+                      :class="{ main_black: stepIndex === 0 }"
+                    >
+                      <strong class="fw-600 mb_5"
+                        >You’ve connected an {{ authChainName }} address</strong
+                      ><br />
+                      <span class="break-all">{{ authAddress }}</span>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="onboarding-between-list-i">
+                    <img
+                      :src="getStepIcon(ESignupStep.verify)"
+                      alt=""
+                      :class="{
+                        'onboarding-between-list-i__img':
+                          stepIndex === 1 && loading,
+                      }"
+                    />
+                  </div>
+                  <div class="onboarding-between-list-t">
+                    <div
+                      class="global-text_14"
+                      :class="{ main_black: stepIndex === 1 }"
+                    >
+                      <strong class="fw-600 mb_5"
+                        >Verify that you own this address</strong
+                      ><br />
+                      You’ll be able to sign in to Crypto.Page using this
+                      address.
+                    </div>
+                    <div class="global-text_12 dark_grey mt_10">
+                      Not seeing the wallet request? Please make sure to open
+                      your wallet extension. If you’re still not seeing it, we
+                      can resend it.
+                      <a href="#" @click.prevent="startStep">Resend request</a>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="onboarding-between-list-i">
+                    <img
+                      :src="getStepIcon(ESignupStep.signMessage)"
+                      alt=""
+                      :class="{
+                        'onboarding-between-list-i__img':
+                          stepIndex === 2 && loading,
+                      }"
+                    />
+                  </div>
+                  <div class="onboarding-between-list-t">
+                    <div
+                      class="global-text_14"
+                      :class="{ main_black: stepIndex === 2 }"
+                    >
+                      Sign the message to get access to Crypto.Page sevices
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="onboarding-between-list-i">
+                    <img
+                      :src="getStepIcon(ESignupStep.consent)"
+                      alt=""
+                      :class="{
+                        'onboarding-between-list-i__img':
+                          stepIndex === 3 && loading,
+                      }"
+                    />
+                  </div>
+                  <div class="onboarding-between-list-t">
+                    <div
+                      class="global-text_14"
+                      :class="{ main_black: stepIndex === 3 }"
+                    >
+                      Consent to register your public profile on Crypto.Page
+                    </div>
+                  </div>
+                </li>
+              </ul>
             </div>
-            <ul class="onboarding-between-list mt_20">
-              <li>
-                <div class="onboarding-between-list-i">
-                  <img
-                    :src="getStepIcon(ESignupStep.connect)"
-                    alt=""
-                    :class="{
-                      'onboarding-between-list-i__img':
-                        stepIndex === 0 && loading,
-                    }"
-                  />
-                </div>
-                <div class="onboarding-between-list-t">
-                  <div
-                    class="global-text_14"
-                    :class="{ main_black: stepIndex === 0 }"
-                  >
-                    <strong class="fw-600 mb_5"
-                      >You’ve connected an {{ authChainName }} address</strong
-                    ><br />
-                    <span class="break-all">{{ authAddress }}</span>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="onboarding-between-list-i">
-                  <img
-                    :src="getStepIcon(ESignupStep.verify)"
-                    alt=""
-                    :class="{
-                      'onboarding-between-list-i__img':
-                        stepIndex === 1 && loading,
-                    }"
-                  />
-                </div>
-                <div class="onboarding-between-list-t">
-                  <div
-                    class="global-text_14"
-                    :class="{ main_black: stepIndex === 1 }"
-                  >
-                    <strong class="fw-600 mb_5"
-                      >Verify that you own this address</strong
-                    ><br />
-                    You’ll be able to sign in to Crypto.Page using this address.
-                  </div>
-                  <div class="global-text_12 dark_grey mt_10">
-                    Not seeing the wallet request? Please make sure to open your
-                    wallet extension. If you’re still not seeing it, we can
-                    resend it.
-                    <a href="#" @click.prevent="startStep">Resend request</a>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="onboarding-between-list-i">
-                  <img
-                    :src="getStepIcon(ESignupStep.signMessage)"
-                    alt=""
-                    :class="{
-                      'onboarding-between-list-i__img':
-                        stepIndex === 2 && loading,
-                    }"
-                  />
-                </div>
-                <div class="onboarding-between-list-t">
-                  <div
-                    class="global-text_14"
-                    :class="{ main_black: stepIndex === 2 }"
-                  >
-                    Sign the message to get access to Crypto.Page sevices
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="onboarding-between-list-i">
-                  <img
-                    :src="getStepIcon(ESignupStep.consent)"
-                    alt=""
-                    :class="{
-                      'onboarding-between-list-i__img':
-                        stepIndex === 3 && loading,
-                    }"
-                  />
-                </div>
-                <div class="onboarding-between-list-t">
-                  <div
-                    class="global-text_14"
-                    :class="{ main_black: stepIndex === 3 }"
-                  >
-                    Consent to register your public profile on Crypto.Page
-                  </div>
-                </div>
-              </li>
-            </ul>
           </div>
         </div>
       </div>
     </div>
+
+    <ConfirmModal
+      ref="confirmModal"
+      @cancel="handleCancelVerify"
+      @accept="handleAcceptVerify"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import { Component, Watch } from 'nuxt-property-decorator';
+import ConfirmModal from './ConfirmModal.vue';
 import SignupModalCloseIcon from '~/components/icon/signup-modal/SignupModalCloseIcon.vue';
 import { authModule } from '~/store';
 import { ESignupStep } from '~/types';
@@ -192,15 +204,19 @@ import { ESignupStep } from '~/types';
 @Component({
   components: {
     SignupModalCloseIcon,
+    ConfirmModal,
   },
 })
 export default class SignupModal extends Vue {
   loading = true;
   ESignupStep = ESignupStep;
   step: ESignupStep = ESignupStep.connect;
+  cancelVerify: any = null;
+  acceptVerify: any = null;
 
   $refs!: {
     modal: HTMLDivElement;
+    confirmModal: ConfirmModal;
   };
 
   get authAddress(): string {
@@ -321,14 +337,29 @@ export default class SignupModal extends Vue {
   }
 
   async startVerify() {
-    this.loading = true;
+    try {
+      this.loading = true;
 
-    const result = await authModule.fractalSign();
+      /** подтверждение старта верификации */
 
-    this.loading = false;
+      await new Promise<void>((resolve, reject) => {
+        this.acceptVerify = resolve;
+        this.cancelVerify = reject;
 
-    if (result) {
-      this.startStep();
+        this.$refs.confirmModal.show();
+      });
+
+      /** проверяем верификацию */
+
+      const result = await authModule.fractalSign();
+
+      this.loading = false;
+
+      if (result) {
+        this.startStep();
+      }
+    } catch {
+      this.loading = false;
     }
   }
 
@@ -338,6 +369,14 @@ export default class SignupModal extends Vue {
 
   startConsent() {
     this.loading = true;
+  }
+
+  handleCancelVerify() {
+    this.cancelVerify && this.cancelVerify();
+  }
+
+  handleAcceptVerify() {
+    this.acceptVerify && this.acceptVerify();
   }
 }
 </script>
