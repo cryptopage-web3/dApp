a6c2382f7462f8d499d31273bd0b711f3edcce11
Nail Mustyukov <nail.mustyukov@kamtor.com>
2023-01-15 22:24:45 +0500

    fix build

--- nuxt.config.js
+++ nuxt.config.js
@@ -90,6 +90,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    postcss: null,
     babel: {
       plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
     },
