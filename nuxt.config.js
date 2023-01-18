27b7c85ef8df560e0e50e77cba522d8296f369da
Nail Mustyukov <nail.mustyukov@kamtor.com>
2023-01-18 07:34:46 +0500

    revert package-lock, nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -90,7 +90,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    postcss: null,
     babel: {
       plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
     },
