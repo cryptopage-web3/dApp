313d42e588989fdb98e32c5751384c4feeffb662
Nail Mustyukov <nail.mustyukov@kamtor.com>
2023-04-14 12:43:41 +0500

    update design

--- _design5/build/js/main.js
+++ _design5/build/js/main.js
@@ -2456,6 +2456,60 @@ if($('#onboarding-modal-between').is('#onboarding-modal-between')) {
 }
 /*start close modal #onboarding-modal-between after time*/
 
+/*start global-request*/
+if($('.request-link').is('.request-link')) {
+	$('.request-link').on('click', function(){
+		event.preventDefault();
+		if($(this).attr('data-id')){
+			$($(this).attr('data-id')).fadeIn(300);
+		}
+		$(this).closest('.global-request').fadeOut(300);
+	});
+}
+/*end global-request*/
+
+/*start timer login page*/
+if($('.sign-variations__input').is('.sign-variations__input')) {
+	function makeTimer(count_seconds) {
+
+		var timeLeft = Number(count_seconds);
+		if(timeLeft == -1) {
+			$('.sign-variations-wrap').addClass('active');
+			return false;
+		}
+		else {
+			var days = Math.floor(timeLeft / 86400); 
+			var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+			var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+			var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+			if (hours < "10") { hours = "0" + hours; }
+			if (minutes < "10") { minutes = "0" + minutes; }
+			if (seconds < "10") { seconds = "0" + seconds; }
+
+			$("#sign-variations__timer-wrap").html(minutes + ":" + seconds);
+			timeLeft = timeLeft - 1;
+			$('.sign-variations__timer').attr('data-time', timeLeft);
+		}
+	}
+	var timerId;
+	var start_time = Number($('.sign-variations__timer').attr('data-time'));
+	$('.send-code').on('click', function(){
+		$('.sign-variations-wrap').slideDown(300);
+		timerId = setInterval(function() { makeTimer($('.sign-variations__timer').attr('data-time')); }, 1000);
+		$(this).closest('div').fadeOut(300);
+	});
+
+	$('.sign-variations__request-code').on('click', function(){
+		event.preventDefault();
+		$('.sign-variations-wrap').removeClass('active');
+		$('.sign-variations__timer').attr('data-time', start_time);
+		setTimeout(() => { clearInterval(timerId)}, 1000);
+		timerId = setInterval(function() { makeTimer($('.sign-variations__timer').attr('data-time')); }, 1000);
+	});
+}
+/*end timer login page*/
+
 });
 
 /*audio init*/
