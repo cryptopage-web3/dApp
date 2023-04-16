c1cf686b553f184d4bf9a31a029e38733cee5412
Nail Mustyukov <nail.mustyukov@kamtor.com>
2023-04-16 07:40:31 +0500

    connect: metamask via single btn

--- components/connect/ConnectMetamask.vue
+++ components/connect/ConnectMetamask.vue
@@ -2,6 +2,7 @@
   <a
     href="#"
     class="sign-variations__metamask btn btn_default btn_extra-large w-100 mb_15"
+    @click.prevent="connectToProvider(EMainChain.eth, EProvider.metamask)"
   >
     <img class="mr_10" src="@/assets/img/sign-variations_img1.svg" alt="" />
     <span> Sign up / Sign in with Metamask </span>
@@ -11,7 +12,38 @@
 <script lang="ts">
 import Vue from 'vue';
 import { Component } from 'nuxt-property-decorator';
+import { EChainSlug, EMainChain, EProvider } from '~/types';
+import { authModule } from '~/store';
+import { networkHelper } from '~/utils/networkHelper';
 
 @Component({})
-export default class ConnectMetamask extends Vue {}
+export default class ConnectMetamask extends Vue {
+  EMainChain = EMainChain;
+  EProvider = EProvider;
+
+  async connectToProvider(chain: EChainSlug, provider: EProvider) {
+    const response = await authModule.connectToProvider({
+      chain,
+      provider,
+    });
+
+    if (response.status === 'error') {
+      this.$notify({
+        type: response.status,
+        title: response.message?.title,
+        text: response.message?.text,
+      });
+
+      return;
+    }
+
+    /** редирект на профиль */
+
+    const { connectData } = response;
+    const address = connectData?.address;
+    const chainSlug = networkHelper.getNetworkSlug(connectData?.chainId);
+
+    this.$router.push(`/${chainSlug}/${address}`);
+  }
+}
 </script>
