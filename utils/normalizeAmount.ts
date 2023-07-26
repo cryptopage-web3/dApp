178d829de5d5be5279d7b0d2cd48d3fd257e89b2
Nail Mustyukov <nail.mustyukov@kamtor.com>
2023-07-26 18:45:35 +0500

    fix normalize amount to server

--- utils/normalizeAmount.ts
+++ utils/normalizeAmount.ts
@@ -1,3 +1,6 @@
-export const normalizeAmountToServer = (amount: number) => amount * 10 ** 18;
+import { BigNumber } from 'ethers';
+
+export const normalizeAmountToServer = (amount: number) =>
+  BigNumber.from(String(amount * 10 ** 18));
 
 export const normalizeAmountToFront = (amount: number) => amount / 10 ** 18;
